a6021bdbf6bff2f7a30828931da70d591687235b
Stephen <subtleargh@gmail.com>
2020-04-26 00:53:30 +0100

    Added helper for reading data from file rather than string

--- sprite_tool/src/file_helper.cpp
+++ sprite_tool/src/file_helper.cpp
@@ -8,7 +8,7 @@
 //========================================
 namespace FileHelper
 {
-	std::string GetFileContents(std::string const& _sFilePath)
+	std::string GetFileContentsString(std::string const& _sFilePath)
 	{
         std::string _sContents;
 
@@ -24,5 +24,22 @@ namespace FileHelper
 
         return _sContents;
 	}
+
+    std::vector<uint8_t> GetFileContents(std::string const& _sFilePath)
+    {
+        std::vector<uint8_t> _Contents;
+
+        std::ifstream _File(_sFilePath, std::ios::in | std::ios::binary);
+        if (_File)
+        {
+            _File.seekg(0, std::ios::end);
+            _Contents.resize(_File.tellg());
+            _File.seekg(0, std::ios::beg);
+            _File.read(reinterpret_cast<char*>(&_Contents[0]), _Contents.size());
+            _File.close();
+        }
+
+        return _Contents;
+    }
 };
 //========================================
--- sprite_tool/src/file_helper.hpp
+++ sprite_tool/src/file_helper.hpp
@@ -1,9 +1,11 @@
 
 #include <string>
+#include <vector>
 
 //========================================
 namespace FileHelper
 {
-	std::string GetFileContents(std::string const &_sFilePath);
+	std::string GetFileContentsString(std::string const &_sFilePath);
+	std::vector<uint8_t> GetFileContents(std::string const& _sFilePath);
 };
 //========================================
